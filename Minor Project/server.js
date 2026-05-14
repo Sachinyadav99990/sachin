65e0c18dc6f77fc1afe73bd8256193d08fbdb94a
sy258 <sy258@local>
2026-05-14 14:47:42 +0530

    Initial commit

---
+++ Minor Project/server.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+// Serve static files (HTML, CSS, JS)
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Route example
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
---
+++ PYTHON/new.py
@@ -0,0 +1,6 @@
+print("my name is sachin")
+name= ("sachin")
+age= float("20")
+price = float("0.999999 only")
+print(name)
+print
